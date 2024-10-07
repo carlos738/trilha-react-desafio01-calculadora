fa9d4cf6db7dbd7f03cfd352f940db2e989e23a3
carlos738 <carlosasl135@hotmail.com>
2024-10-07 21:44:22 +0000

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,15 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #CACACA;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #873e23;
+    background-color: #1e81b0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
 `
-
-
-export const Content = styled.div`
-    background-color: #FFFFFF;
-    width: 50%;
-`
-
-export const Row = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-`
-
-export const Column = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
-
